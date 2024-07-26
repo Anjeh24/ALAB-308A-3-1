c4b6ef4d2599cb243f6836272a268892701f1c01
anjeh24 <junez24@yahoo.com>
2024-07-25 23:25:12 -0400

    trying to figure out function set up and async await function to go with it

--- scripts/index.js
+++ scripts/index.js
@@ -4,29 +4,36 @@ import { central, db1, db2, db3, vault } from "./databases.js";
 let appdiv = document.getElementById('app')
 
 
-function getUserData(id) {
-  const dbs = {
+ function getUserData(id) {
+   const dbs = {
     db1: db1,
     db2: db2,
     db3: db3
   };
-   if ((id > 1) && (id < 4)){
-    return db1;
+    if ((id > 1) && (id < 4)){
+        setTimeout(function(){return db1}, 100); //if conditions are met, try to get users data within one onehundreth of a second etc
+    
    } else if 
      ((id > 4) && (id < 7)){
-        return db2;
+        setTimeout(function(){return db2}, 200);
    } else if ((id > 7) && (id < 10)){
-    return db3;
+    setTimeout(function(){return db3}, 200);
    } else {
     return (`${id} is not recognized in our database`);
    }
-
-
+  
 }
 
-getUserData();
+async function displayData(){ //async function to execute once conditions are met. Well, I tried to figure out a strategy / solution thogh not perfect.
+  let userData =   await getUserData(id); 
+  console.log(userData);
+}
+displayData();
 
-console.log(getUserData);
+
+//getUserData();
+
+//console.log(getUserData);
 
 //appdiv.innerHTML = getUserData
 
